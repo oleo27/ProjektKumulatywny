11908a0d7fe97df04b3fa42b229d77d4ff79c9f5
Olek <olek.grajzer@gmail.com>
2025-11-23 20:11:03 +0100

    Quiz, Question and Answer classes added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+	class Question
+	{
+		public string Text { get; set; }
+		public List<Answer> Answers { get; set; }
+		public Question(string text)
+		{
+			Text = text;
+			Answers = new List<Answer>();
+		}
+		public void addAnswers(Answer answer)
+		{
+			Answers.Add(answer);
+		}
+
+	}
+
+	class Answer
+	{
+		public string Text { get; set; }
+		public bool IsCorrect { get; set; }
+		public Answer(string text, bool isCorrect)
+		{
+			Text = text;
+			IsCorrect = isCorrect;
+		}
+	}
+
+	class Quiz
+	{
+		public string Title { get; set; }
+		public List<Question> Questions { get; set; }
+		public Quiz(string title)
+		{
+			Title = title;
+		}
+
+		public void addQuestion(Question question)
+		{
+			Questions.Add(question);
+		}
+	}
+	private static void Main(string[] args)
+	{
+		
+	}
+}

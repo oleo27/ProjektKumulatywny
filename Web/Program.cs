b28a6594e338c9bed70f4bfdcaa30b41cd746e9b
Olek <olek.grajzer@gmail.com>
2026-01-13 21:15:12 +0100

    Web App Core added, Db conf

---
+++ Web/Program.cs
@@ -0,0 +1,33 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<QuizContext>(options =>
+	options.UseSqlServer(
+		"Server=(localdb)\\MSSQLLocalDB;Database=QuizDb;Trusted_Connection=True;"
+	));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
